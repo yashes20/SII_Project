62faa5f511a965d317eead9a7f8d219a4d853657
Yasmin Hage <74877135+yashes20@users.noreply.github.com>
2022-07-16 17:16:55 +0100

    rating getRequestsByTaskId

--- scripts/request-handlers.js
+++ scripts/request-handlers.js
@@ -24,10 +24,8 @@ const queryAllTasks = "SELECT taskId, taskName, taskDescription,taskDateCreation
 
 const queryAllRequests = "SELECT requestId, requestIdVoluntary, requestIdTask,  cast(requestStatus as UNSIGNED) requestStatus FROM requests";
 
-const queryAllRequestsByTaskId = "SELECT req.requestId, tasks.taskId, tasks.taskName,  users.userId, users.userFullName, users.userEmail, users.userPhone, cast(req.requestStatus as UNSIGNED) requestStatus, " +
-    "(SELECT  IFNULL(round(sum(rating) / count(1)),0) "+
-    " FROM ratings "+
-    " WHERE ratings.ratingIdUser = users.userId ) rating " +
+const queryAllRequestsByTaskId = "SELECT req.requestId, tasks.taskId, tasks.taskName, "+
+    "users.userId, users.userFullName, users.userEmail, users.userPhone, cast(req.requestStatus as UNSIGNED) requestStatus, users.rating " +
     "from requests req " +
     "INNER JOIN tasks ON tasks.taskId = req.requestIdTask " +
     "INNER JOIN users ON users.userId = req.requestIdVoluntary " +
